a6f5fabead11145f055d6d60d28d480611d31bf1
Skinick <107771280+Skinick@users.noreply.github.com>
2023-07-31 00:18:05 +0300

    Edit App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import css from './App.module.css';
 import { nanoid } from 'nanoid';
 
@@ -7,42 +7,25 @@ import ContactList from './ContactList';
 import Filter from './Filter';
 import Notification from './Notification';
 
-class App extends Component {
-  state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+function App() {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(window.localStorage.getItem('contacts')) ?? []
+  );
+  const [filter, setFilter] = useState('');
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  isDublicate(name) {
+  const isDublicate = name => {
     const normalizedName = name.toLowerCase();
-    const { contacts } = this.state;
     const result = contacts.find(({ name }) => {
       return name.toLowerCase() === normalizedName;
     });
     return Boolean(result);
-  }
+  };
 
-  addContact = ({ name, number }) => {
-    if (this.isDublicate(name)) {
+  const addContact = ({ name, number }) => {
+    if (isDublicate(name)) {
       alert(`${name} is already in contacts`);
       return false;
     }
@@ -53,54 +36,46 @@ class App extends Component {
       number: number,
     };
 
-    this.setState(prevstate => ({
-      contacts: [...prevstate.contacts, contact],
-    }));
+    setContacts(prevContacts => [...prevContacts, contact]);
     return true;
   };
 
-  changeFilter = event => {
-    this.setState({ filter: event.currentTarget.value });
+  const changeFilter = event => {
+    setFilter(event.currentTarget.value.trim());
   };
 
-  getVisisbleContacts = () => {
-    const { filter, contacts } = this.state;
+  const getVisisbleContacts = () => {
     const normalizedFilter = filter.toLocaleLowerCase();
     return contacts.filter(contact =>
       contact.name.toLocaleLowerCase().includes(normalizedFilter)
     );
   };
 
-  deleteContact = todoId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== todoId),
-    }));
+  const deleteContact = todoId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== todoId)
+    );
   };
 
-  render() {
-    const { contacts, filter } = this.state;
+  return (
+    <div className={css.phonebookContainer}>
+      <h1 className={css.titlePhonebook}>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
 
-    return (
-      <div className={css.phonebookContainer}>
-        <h1 className={css.titlePhonebook}>Phonebook</h1>
-        <ContactForm onSubmit={this.addContact} />
-
-        <h2 className={css.titleContacts}>Contacts</h2>
-        <div className={css.allContacts}>All contacts: {contacts.length}</div>
-        {contacts.length > 0 ? (
-          <>
-            <Filter value={filter} onChange={this.changeFilter} />
-            <ContactList
-              contacts={this.getVisisbleContacts()}
-              onDeleteContact={this.deleteContact}
-            />
-          </>
-        ) : (
-          <Notification message="Contact list is empty" />
-        )}
-      </div>
-    );
-  }
+      <h2 className={css.titleContacts}>Contacts</h2>
+      <div className={css.allContacts}>All contacts: {contacts.length}</div>
+      {contacts.length > 0 ? (
+        <>
+          <Filter value={filter} onChange={changeFilter} />
+          <ContactList
+            contacts={getVisisbleContacts()}
+            onDeleteContact={deleteContact}
+          />
+        </>
+      ) : (
+        <Notification message="Contact list is empty" />
+      )}
+    </div>
+  );
 }
-
 export default App;
